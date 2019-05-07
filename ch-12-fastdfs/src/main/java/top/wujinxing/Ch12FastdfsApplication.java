0f2be467d92d39dafc4a580978590dcf649bbf35
kingstar718 <936016045@qq.com>
2019-05-07 09:51:23 +0800

    change

---
+++ ch-12-fastdfs/src/main/java/top/wujinxing/Ch12FastdfsApplication.java
@@ -0,0 +1,13 @@
+package top.wujinxing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch12FastdfsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch12FastdfsApplication.class, args);
+    }
+
+}
---
+++ ch-9-security/src/main/java/cn/kingstar/Ch9SecurityApplication.java
@@ -0,0 +1,12 @@
+package cn.kingstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch9SecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch9SecurityApplication.class, args);
+    }
+}
