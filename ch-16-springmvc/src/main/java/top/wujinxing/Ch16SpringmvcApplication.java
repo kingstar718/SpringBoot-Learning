c1c3080ac2afbe97fbe404dfcaf1305991d22465
kingstar718 <936016045@qq.com>
2019-05-29 18:01:30 +0800

    springmvc

---
+++ ch-16-springmvc/src/main/java/top/wujinxing/Ch16SpringmvcApplication.java
@@ -0,0 +1,13 @@
+package top.wujinxing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch16SpringmvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch16SpringmvcApplication.class, args);
+    }
+
+}
---
+++ ch-16-springmvc/src/main/java/top/wujinxing/controller/MyController.java
@@ -0,0 +1,51 @@
+package top.wujinxing.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author wujinxing
+ * date 2019 2019/5/29 17:42
+ * description
+ */
+@RequestMapping("/my")
+@Controller
+public class MyController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MyController.class);
+
+    //无须任何注解的情况下 ，就要求 HTTP 参数和控制器方法参数名称保持一致。
+    //参数可以不传, 就默认为空
+    @GetMapping("/no/annotation")
+    @ResponseBody
+    public Map<String, Object> noAnnotation(Integer intVal, Long longVal, String str){
+        Map<String, Object> paramsMap = new HashMap<>();
+        paramsMap.put("intVal", intVal);
+        paramsMap.put("longVal", longVal);
+        paramsMap.put("str", str);
+        LOGGER.info(paramsMap.toString());
+        return paramsMap;
+    }
+
+    @GetMapping("/annotation")
+    @ResponseBody
+    public Map<String, Object> requestParam(
+            @RequestParam("int_val") Integer intVal,
+            @RequestParam("long_val") Long longVal,
+            @RequestParam("str_val") String strVal){
+        Map<String, Object> paramsMap = new HashMap<>();
+        paramsMap.put("intVal", intVal);
+        paramsMap.put("longVal", longVal);
+        paramsMap.put("str", strVal);
+        LOGGER.info(paramsMap.toString());
+        return paramsMap;
+    }
+}
